4db156198144bc338ce13b6cc1ea32df19537f86
Harsh Bhatnagar <harshbhatnagar@harshs-macbook-pro.local>
2019-04-18 23:24:58 +0530

    Initial Commit

---
+++ SSHProjectTests/SSHProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  SSHProjectTests.swift
+//  SSHProjectTests
+//
+//  Created by Harsh Bhatnagar on 18/04/19.
+//  Copyright © 2019 SSH. All rights reserved.
+//
+
+import XCTest
+@testable import SSHProject
+
+class SSHProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
